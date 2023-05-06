49dfa89b84d5c62c8cf3dc1c49649ca15c45265e
AJ-6399 <128492801+AJ-6399@users.noreply.github.com>
2023-05-07 00:59:52 +0100

    Added comments to frontend components

--- src/Components/Itinerary.js
+++ src/Components/Itinerary.js
@@ -1,9 +1,8 @@
 import React,{useState,useEffect} from 'react'
-
 export default function Itinerary() {
-    const [title,setTitle]=useState([])
-    const [url,setUrl]=useState([])
-    const [description,setDescription]=useState([])
+    const [title,setTitle]=useState([])//to use setTitle to change state of title
+    const [url,setUrl]=useState([])//to use setUrl to change state of url
+    const [description,setDescription]=useState([])//to use setDescription to change state of description
   
 useEffect(() => {
          setTitle(localStorage.getItem("title"));
@@ -23,13 +22,13 @@ useEffect(() => {
   </div>
     </div>
   </div>
-  <svg xmlns="http://www.w3.org/2000/svg" style={{color:'white'}}  width="50" height="50" fill="currentColor" className="bi bi-chevron-compact-down my-3" viewBox="0 0 16 16">
+  <svg xmlns="http://www.w3.org/2000/svg" style={{color:'white'}} width="50" height="50" fill="currentColor" className="bi bi-chevron-compact-down my-3" viewBox="0 0 16 16">
   <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
 </svg>
 
   <h3 className='my-3' style={{color:"orange",fontWeight:"950",fontSize:30}}>Itinerary</h3>
 <div className='container my-3'>
-<p style={{fontSize:20,fontWeight:"550",color:'white'}}>{description} </p>
+<p style={{fontSize:20,fontWeight:"550",color:'white'}}>{description}</p>
 </div>
 </div>
 </center>
